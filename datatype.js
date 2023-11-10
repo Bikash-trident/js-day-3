e2ae9e3d82e9d349cca3e5258462c93bab10a46e
Bikah-trident <bikashnayak0973@gmail.com>
2023-11-10 14:50:25 +0530

    Day3

---
+++ datatype.js
@@ -0,0 +1,14 @@
+//string
+let x="bikash";
+//number
+let y=8;
+//boolean
+let a=true;
+let b=false;
+//object
+const person={firstName:"Bikash",lastName:"Nayak"};
+//arrays
+const cars=["maruti","audi"];
+//Date object
+const date=new Date("01/05/2000");
+console.log(typeof(person));
